45fae69223903f7f3f9fa3b602da1b71395f03e1
dapal19 <55704032+dapal19@users.noreply.github.com>
2021-11-28 20:27:06 +0100

    Nu kan man oprette en bruger

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 const express = require ("express")
 const app = express()
 
+// Indhenter bycrypt
+// Bcrypt gør det muligt for os at hashe passwords
+// lader ikke til at være ikke krav dog
+const bcrypt = require ("bcrypt")
+
 //Vi laver en konstant som tager fat i input
 const brugere = []
 
@@ -19,24 +24,34 @@ app.get("/", (req,res) => {
     res.render("index.ejs",)
 })
 app.post("/", (req,res) => {
-    
 })
-
-
 //Laver en get-request for login 
 app.get("/login", (req,res) => {
     res.render("login.ejs",)
 })
-
 app.post("/login", (req,res) => {
-    
 })
-
-
 //Laver en get-request for opret bruger
 app.get("/opretbruger", (req,res) => {
     res.render("opretbruger.ejs",)
 })
-app.post("/opretbruger", (req,res) => {
+//Laver en post-request som pusher oplysninger fra brugeren til vores array brugere
+// Her implementeres der desuden bcrypt, så brugerens password bliver hashed.
+app.post("/opretbruger", async (req ,res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        brugere.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        //Efterfølgende redirecter vi til vores login-side.
+        res.redirect("/login")
+        //Vi console.logger, så vi kan se om vi har tilføjet en bruger.
+        console.log(brugere)
+    }catch{
+        res.redirect("/opretbruger")
+    }})
     
-})
+  
