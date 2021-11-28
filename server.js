b70582d8f9d280bfbb1f114e07bca550322806bc
dapal19 <55704032+dapal19@users.noreply.github.com>
2021-11-28 19:34:02 +0100

    Login og opret bruger

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require ("express")
 const app = express()
 
+//Vi laver en konstant som tager fat i input
+const brugere = []
+
 //Gør at vi kan bruge ejs syntax
 app.set("view-engine", "ejs")
 
@@ -11,18 +14,29 @@ app.use(express.urlencoded({extended:false}))
 //Lyder til localhost 5000
 app.listen(5000)
 
-
 //Laver en get-request for / 
 app.get("/", (req,res) => {
     res.render("index.ejs",)
 })
+app.post("/", (req,res) => {
+    
+})
+
 
 //Laver en get-request for login 
 app.get("/login", (req,res) => {
     res.render("login.ejs",)
 })
 
+app.post("/login", (req,res) => {
+    
+})
+
+
 //Laver en get-request for opret bruger
 app.get("/opretbruger", (req,res) => {
     res.render("opretbruger.ejs",)
+})
+app.post("/opretbruger", (req,res) => {
+    
 })
